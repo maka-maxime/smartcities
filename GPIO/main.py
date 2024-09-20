2189624a93adad993edda2cf01c7e88a00e5cd14
Maxime Maka <maxime.maka@student.hepl.be>
2024-09-20 17:15:33 +0200

    GPIO v0.3 - clignotement à période variable

--- GPIO/main.py
+++ GPIO/main.py
@@ -1,12 +1,24 @@
-from machine import Pin
-import utime
+from machine import Pin, Timer
 
 led = Pin(16, Pin.OUT)
 led.value(0)
 
-def toggle_led(led):
+def toggle_led(t):
+    global led
+    global count
     led.value(led.value() ^ 1)
+    count += 1
+
+timer = Timer(-1)
+periods = [2000, 500]
+state = 0
+count = 0
+    
+timer.init(period=periods[state],mode=Timer.PERIODIC,callback=toggle_led)
 
 while True:
-    toggle_led(led)
-    utime.sleep(1)
+    if count == 6:
+        timer.deinit()
+        count = 0
+        state = (state + 1) % 2
+        timer.init(period=periods[state],mode=Timer.PERIODIC,callback=toggle_led)
