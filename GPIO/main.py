691dd28998514be2d7ae076d57f8759dadc1b5ab
Maxime Maka <maxime.maka@student.hepl.be>
2024-09-20 17:09:00 +0200

    GPIO v0.2 - clignotement simple

--- GPIO/main.py
+++ GPIO/main.py
@@ -1,1 +1,12 @@
-print('hello micropython!')
+from machine import Pin
+import utime
+
+led = Pin(16, Pin.OUT)
+led.value(0)
+
+def toggle_led(led):
+    led.value(led.value() ^ 1)
+
+while True:
+    toggle_led(led)
+    utime.sleep(1)
