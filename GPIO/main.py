5ab190342bf248e1c13ccfcbd9fe77f6d007e5c7
Maxime Maka <maxime.maka@student.hepl.be>
2024-09-22 22:24:43 +0200

    GPIO v0.4 - gestion d'un bouton par interruption

--- GPIO/main.py
+++ GPIO/main.py
@@ -6,14 +6,31 @@ led.value(0)
 def toggle_led(t):
     global led
     global count
-    led.value(led.value() ^ 1)
+    led.toggle()
     count += 1
 
 timer = Timer(-1)
 periods = [2000, 500]
 state = 0
 count = 0
-    
+
+button_state = 0
+mutex_button = 0
+def button_handler(pin):
+    button.irq(handler=None)
+    global mutex_button
+    global button_state
+    if button.value() == 1 and mutex_button == 0:
+        mutex_button = 1
+        button_state = (button_state + 1) % 3
+        print(f"{button_state}")
+    elif button.value() == 0 and mutex_button == 1:
+        mutex_button = 0
+    button.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=button_handler)
+
+button = Pin(18, Pin.IN, Pin.PULL_DOWN)
+button.irq(trigger=Pin.IRQ_RISING, handler=button_handler)
+
 timer.init(period=periods[state],mode=Timer.PERIODIC,callback=toggle_led)
 
 while True:
