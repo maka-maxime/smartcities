1cb58f49727148680defc95b5339fb21ed8492ad
Maxime Maka <maxime.maka@student.hepl.be>
2024-09-23 09:25:14 +0200

    GPIO v0.5 - contrôle de la fréquence de clignotement de la led avec le bouton

--- GPIO/main.py
+++ GPIO/main.py
@@ -1,41 +1,37 @@
 from machine import Pin, Timer
+import utime
+
+reference_ticks = 0
+event_ticks = 0
+DELTA_T = 200
 
 led = Pin(16, Pin.OUT)
 led.value(0)
 
-def toggle_led(t):
-    global led
-    global count
-    led.toggle()
-    count += 1
-
 timer = Timer(-1)
 periods = [2000, 500]
-state = 0
-count = 0
 
 button_state = 0
-mutex_button = 0
 def button_handler(pin):
     button.irq(handler=None)
-    global mutex_button
     global button_state
-    if button.value() == 1 and mutex_button == 0:
-        mutex_button = 1
+    global reference_ticks
+    global event_ticks
+    global timer
+    event_ticks = utime.ticks_ms()
+    print(f"ref: {reference_ticks}, evt:{event_ticks}")
+    if event_ticks - reference_ticks > DELTA_T:
+        timer.deinit()
+        led.value(0)
+        reference_ticks = event_ticks
+        event_ticks = utime.ticks_ms()
         button_state = (button_state + 1) % 3
-        print(f"{button_state}")
-    elif button.value() == 0 and mutex_button == 1:
-        mutex_button = 0
-    button.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=button_handler)
+        if button_state is not (0 or 3):
+            timer.init(period=periods[button_state-1],mode=Timer.PERIODIC,callback=lambda t:led.toggle())
+    button.irq(trigger=Pin.IRQ_RISING, handler=button_handler)
 
 button = Pin(18, Pin.IN, Pin.PULL_DOWN)
 button.irq(trigger=Pin.IRQ_RISING, handler=button_handler)
 
-timer.init(period=periods[state],mode=Timer.PERIODIC,callback=toggle_led)
-
 while True:
-    if count == 6:
-        timer.deinit()
-        count = 0
-        state = (state + 1) % 2
-        timer.init(period=periods[state],mode=Timer.PERIODIC,callback=toggle_led)
+    utime.sleep_us(100)
