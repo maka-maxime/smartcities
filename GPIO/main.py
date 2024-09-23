bcce9378312552f487de311fb17b7d03f552e415
Maxime Maka <maxime.maka@student.hepl.be>
2024-09-23 09:59:10 +0200

    GPIO v0.6 - ajout d'une animation de lors du changement de fréquence

--- GPIO/main.py
+++ GPIO/main.py
@@ -9,7 +9,18 @@ led = Pin(16, Pin.OUT)
 led.value(0)
 
 timer = Timer(-1)
-periods = [2000, 500]
+periods = [2000, 200, 100, 50]
+STATES = len(periods)+1
+
+def update_animation(led, state):
+    HALF_T = 200
+    utime.sleep_ms(2*HALF_T)
+    for i in range(state):
+        led.toggle()
+        utime.sleep_ms(HALF_T)
+        led.toggle()
+        utime.sleep_ms(HALF_T)
+    utime.sleep_ms(2*HALF_T)
 
 button_state = 0
 def button_handler(pin):
@@ -25,9 +36,11 @@ def button_handler(pin):
         led.value(0)
         reference_ticks = event_ticks
         event_ticks = utime.ticks_ms()
-        button_state = (button_state + 1) % 3
-        if button_state is not (0 or 3):
+        button_state = (button_state + 1) % STATES
+        if button_state is not 0:
+            update_animation(led, button_state)
             timer.init(period=periods[button_state-1],mode=Timer.PERIODIC,callback=lambda t:led.toggle())
+            led.value(1)
     button.irq(trigger=Pin.IRQ_RISING, handler=button_handler)
 
 button = Pin(18, Pin.IN, Pin.PULL_DOWN)
